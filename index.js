7389cc5658c66b97ad88c632ea3c38e030d0cf2d
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-10 13:44:50 +0200

    set cors origin

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 import "./config/config.js";
-import {emailAvailable, register} from "./controller/userController.js"
-import {encryptPassword} from "./middlewares/authMiddleware.js"
+import { emailAvailable, register } from "./controller/userController.js";
+import { encryptPassword } from "./middlewares/authMiddleware.js";
 import { login } from "./controller/loginController.js";
 import { deleteCookieMiddleware } from "./middlewares/deleteCookieMiddleware.js";
 import { logout } from "./controller/logoutController.js";
@@ -15,36 +15,25 @@ const app = express();
 const PORT = process.env.PORT || 8989;
 
 //middleware
-app.use(cors({  credentials: true}));    // as damit https Cookie vom Fontend angenommen wird
+app.use(cors({ origin: true, credentials: true })); // as damit https Cookie vom Fontend angenommen wird
 
-
-const upload = multer() 
+const upload = multer();
 // app.use(multer());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan("dev"));
 
-
 // routes
 
-app.post('/api/v1/login', encryptPassword,login)    // as encryptPassword müsste von Lando von der Regestrierung kommen
-// ! Vorsicht token ist wegen Testen auf 20 Tage gesetzt 
+app.post("/api/v1/login", encryptPassword, login); // as encryptPassword müsste von Lando von der Regestrierung kommen
+// ! Vorsicht token ist wegen Testen auf 20 Tage gesetzt
 
 // verifyJWTTokenMiddleware    // as für die Routen
 
+app.get("/api/v1/availiable", emailAvailable);
+app.post("/api/v1/register", encryptPassword, register);
 
-
-
-app.get('/api/v1/availiable',emailAvailable)
-app.post('/api/v1/register', encryptPassword,register)
-
-
-
-
-
-
-app.get('/', deleteCookieMiddleware, logout) // as für logout
-
+app.get("/", deleteCookieMiddleware, logout); // as für logout
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
 // test ob render jetzt geht
