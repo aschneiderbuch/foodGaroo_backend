c453a149366dfea865a5620e8f7bd35de07ef543
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-12 09:56:24 +0200

    saved index

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ import "./config/config.js";
 import { emailAvailable, register } from "./controller/userController.js";
 import { encryptPassword } from "./middlewares/authMiddleware.js";
 import { login } from "./controller/loginController.js";
-import { deleteCookieMiddleware } from "./middlewares/deleteCookieMiddleware.js";
 import { logout } from "./controller/logoutController.js";
 import { verifyJWTTokenMiddleware } from "./middlewares/verifyJWTTokenMiddleware.js";
 import { cloudinaryUpload } from "./controller/cloudinaryUploadController.js";
@@ -15,7 +14,6 @@ import { multerCloudinaryOptionsController } from "./controller/multerCloudinary
 import { getProfileId } from "./controller/getProfileIdController.js";
 import { search } from "./controller/searchController.js";
 import { mongoDbFilterQueryController } from "./controller/mongoDbFilterQueryController.js";
-
 
 const app = express();
 
@@ -25,7 +23,7 @@ const PORT = process.env.PORT || 8989;
 app.use(cors({ origin: true, credentials: true })); // as damit https Cookie vom Fontend angenommen wird
 
 const upload = multer();
-const uploadCloudinary = multer( multerCloudinaryOptionsController)
+const uploadCloudinary = multer(multerCloudinaryOptionsController);
 
 app.use(cookieParser());
 app.use(express.json());
@@ -33,29 +31,31 @@ app.use(morgan("dev"));
 
 // routes
 
-app.post("/api/v1/login", encryptPassword, login); 
+app.post("/api/v1/login", encryptPassword, login);
 // ! Vorsicht token ist wegen Testen auf 20 Tage gesetzt
-
 
 app.get("/api/v1/availiable", emailAvailable);
 app.post("/api/v1/register", encryptPassword, register);
 
 // ! dann noch      verifyJWTTokenMiddleware  mit rein // as für die Route
-app.post('/api/v1/directupload', verifyJWTTokenMiddleware, uploadCloudinary.single('file'), cloudinaryUpload); 
+app.post(
+	"/api/v1/directupload",
+	verifyJWTTokenMiddleware,
+	uploadCloudinary.single("file"),
+	cloudinaryUpload
+);
 
-app.get('/api/v1/filter', mongoDbFilterQueryController)
+app.get("/api/v1/filter", mongoDbFilterQueryController);
 
-app.get('/api/v1/profile',verifyJWTTokenMiddleware, getProfileId)
+app.get("/api/v1/profile", verifyJWTTokenMiddleware, getProfileId);
 
+app.get("/api/v1/logout", verifyJWTTokenMiddleware, logout);
 
-app.get("/api/v1/logout",verifyJWTTokenMiddleware, deleteCookieMiddleware, logout); 
+app.get("/api/v1/verify", verifyJWTTokenMiddleware, (_, res) => {
+	res.end();
+});
 
-app.get('/api/v1/verify', verifyJWTTokenMiddleware, (_,res)=> {
-    res.end()
-})
-
-app.get('/api/v1/search', search)
-
+app.get("/api/v1/search", search);
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
 // test ob render jetzt geht
