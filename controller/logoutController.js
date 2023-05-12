9001a2f369d855a32d40c910014500d3b67a73f2
Karin Lee <119490837+kleeblattdev@users.noreply.github.com>
2023-05-12 09:50:18 +0200

    fixed logoutController

--- controller/logoutController.js
+++ controller/logoutController.js
@@ -1,5 +1,3 @@
-
-
 export const logout = async (req, res) => {
-    res.end()
-}
+	res.clearCookie("token").end();
+};
--- middlewares/deleteCookieMiddleware.js
+++
@@ -1,7 +0,0 @@
-
-
-export const deleteCookieMiddleware = (req, res, next) => {
-    res.clearCookie('token')
-
-    next()
-}
