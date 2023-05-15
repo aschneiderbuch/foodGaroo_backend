0101045cdd4b1fa069100c5a16e4cc2f9032ece0
Lando Meyer <121017979+LandoADeveloper@users.noreply.github.com>
2023-05-15 11:54:54 +0200

    added cartController and routes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ import { getCategories, getProductsFromCategories, search, getProductDetails } f
 import { mongoDbFilterQueryController } from "./controller/mongoDbFilterQueryController.js";
 import { getBadges } from "./controller/getBadges.js";
 import { postUserInputDatenUpdaten } from "./controller/postUserInputDatenUpdaten.js";
+import { getCart, addItemToCart, deleteItemFromCart } from "./controller/cartController.js";
 
 const app = express();
 
@@ -64,6 +65,11 @@ app.get("/api/v1/product/:id", getProductDetails)
 
 app.get('/api/v1/badges',verifyJWTTokenMiddleware, getBadges)
 app.put('/api/v1/editUserProfile',verifyJWTTokenMiddleware,  postUserInputDatenUpdaten)
+
+app.get('/api/v1/cart',verifyJWTTokenMiddleware, getCart)
+app.post('/api/v1/cart',verifyJWTTokenMiddleware, addItemToCart)
+app.delete('/api/v1/cart',verifyJWTTokenMiddleware, deleteItemFromCart)
+
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
 // test ob render jetzt geht
