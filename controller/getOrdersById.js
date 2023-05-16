d739bd9220a45f9b7a49c4905895ad9cf8622815
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-05-17 00:38:20 +0200

    bE testing    Order Details läuft

--- controller/getOrdersById.js
+++ controller/getOrdersById.js
@@ -5,16 +5,18 @@ import { ObjectId } from 'mongodb'
 const COL= 'orders'
 export const getOrdersById = async (req, res) => {
     try {
-        const id = req.params.id
+        const idUser = req.user.user
+        const idParamsItem = req.params.id
+        console.log(idUser)
+        console.log(idParamsItem) // = _id der Order
 
-        if (!id) return res.status(504).end()
-        console.log(id);
+        if (!idParamsItem) return res.status(504).end()
 
         const db = await getDB()
 
-        const orders = await db.collection(COL).find({ _id: new ObjectId(id) }).toArray()
-
-        res.status(200).json(orders)
+        const orders = await db.collection(COL).find({ _id: new ObjectId(idParamsItem) }).toArray()
+        console.log(orders);
+        res.status(200).json(orders).end()
 
     } catch (err) {
         console.log(err, 598);
